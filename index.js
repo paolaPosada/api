3831093cc22808b1be9096361bff25fd131ac4a9
Paola <paola.posada1@udea.edu.co>
2020-02-27 16:53:22 -0500

    meh

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const app = require('./app')
 //Importar modelo
 
 const port = process.env.PORT || 3001
-mongoose.connect('mongodb+srv://sht:nijOcHQF7ixdZWc4@cluster0-7av5w.mongodb.net/smartparking?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://paola:m4irjW5uHQOCL3P9@cluster0-7av5w.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
   })
   .then(() =>{
-  console.log("DB connection successful!")
-  app.listen(port, () => {
-    console.log(`API REST corriendo en http://localhost:${port}`);
-  })}
+    console.log("DB connection successful!")
+    app.listen(port, () => {
+      console.log(`API REST corriendo en http://localhost:${port}`);
+    })}
   )
   .catch(error => {
     console.log(error);
